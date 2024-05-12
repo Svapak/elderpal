a52672f3a96909098aaa08a10106d3964764b915
Swapak <113049770+Svapak@users.noreply.github.com>
2024-05-12 11:05:03 +0530

    Delete app directory

--- app/src/main/java/com/explore/eldercare/ui/donation/DonationData.kt
+++
@@ -1,9 +0,0 @@
-package com.explore.eldercare.ui.donation
-
-data class DonationData (
-    val name: String="",
-    val description:String="",
-    val contact : String="",
-    val address : String="",
-    val occupants:String=""
-)
--- app/src/main/java/com/explore/eldercare/ui/models/OldageHome.kt
+++
@@ -1,9 +0,0 @@
-package com.explore.eldercare.ui.models
-
-data class OldageHome(
-    val name : String? = "",
-    val occupants :String? = "",
-    val description: String? = "",
-    val address: String? ="",
-    val contact: String?= ""
-)
